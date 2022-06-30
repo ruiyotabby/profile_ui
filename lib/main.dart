13f12bc23f3876c8314b68a2b1cc4379ef114086
Ruiyot Abby <ruiyotabby@gmail.com>
2022-06-30 20:31:24 +0300

    commit
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.grey[800],
       body: Center(
         child: Column(
           children: [],
