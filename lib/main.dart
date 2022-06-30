66f2d0b7e447b0d8d163fe21d37ffdadc2406bd3
Ruiyot Abby <ruiyotabby@gmail.com>
2022-06-30 21:08:31 +0300

    commit
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
       backgroundColor: Colors.grey[800],
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 70,
-              backgroundImage: AssetImage('assets/images/omosh_copy.jpeg'),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 70,
+                backgroundImage: AssetImage('assets/images/omosh_copy.jpeg'),
+              ),
+              Text('helo', style: GoogleFonts.lato(color: Colors.white, fontSize: 24,),),
+            ],
+          ),
         ),
       ),
     ),
