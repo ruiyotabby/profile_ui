fa99628ab34cb88d26c70bad8ce70bb0c851b238
Ruiyot Abby <ruiyotabby@gmail.com>
2022-07-01 14:46:05 +0300

    commit
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -7,83 +7,92 @@ void main() {
         backgroundColor: Colors.grey[900],
         body: Stack(
           children: [
+            Positioned(
+              top: 50,
+              left: 20,
+              child: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              ),
+            ),
             Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 70,
-                backgroundImage: AssetImage('assets/images/omosh_copy.jpeg'),
-              ),
-              SizedBox(height: 7),
-              Text(
-                'Ruiyot Abby',
-                style: GoogleFonts.lato(
-                  color: Colors.white,
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 70,
+                      backgroundImage:
+                          AssetImage('assets/images/omosh_copy.jpeg'),
+                    ),
+                    SizedBox(height: 7),
+                    Text(
+                      'Ruiyot Abby',
+                      style: GoogleFonts.lato(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'ruiyotabby@gmail.com',
+                      style: GoogleFonts.robotoMono(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Container(
+                      height: 50,
+                      width: 290,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow[500],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Upgrade to premium',
+                          style: GoogleFonts.robotoMono(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 25),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ProfileMenuItem(
+                      text: 'Your order history',
+                      icon: Icons.shopping_bag_outlined,
+                      iconShown: true,
+                    ),
+                    SizedBox(height: 10),
+                    ProfileMenuItem(
+                      text: 'Help and support',
+                      icon: Icons.help_outline,
+                      iconShown: true,
+                    ),
+                    SizedBox(height: 10),
+                    ProfileMenuItem(
+                      text: 'Load gift voucher',
+                      icon: Icons.card_giftcard_outlined,
+                      iconShown: true,
+                    ),
+                    SizedBox(height: 10),
+                    ProfileMenuItem(
+                      text: 'Logout',
+                      icon: Icons.logout,
+                      iconShown: false,
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 5),
-              Text(
-                'ruiyotabby@gmail.com',
-                style: GoogleFonts.robotoMono(
-                  color: Colors.white,
-                  fontSize: 15,
-                  fontStyle: FontStyle.italic,
-                ),
-              ),
-              SizedBox(height: 5),
-              Container(
-                height: 50,
-                width: 290,
-                decoration: BoxDecoration(
-                  color: Colors.yellow[500],
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(30),
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    'Upgrade to premium',
-                    style: GoogleFonts.robotoMono(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 25),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              ProfileMenuItem(
-                text: 'Your order history',
-                icon: Icons.shopping_bag_outlined,
-                iconShown: true,
-              ),
-              SizedBox(height: 10),
-              ProfileMenuItem(
-                text: 'Help and support',
-                icon: Icons.help_outline,
-                iconShown: true,
-              ),
-              SizedBox(height: 10),
-              ProfileMenuItem(
-                text: 'Load gift voucher',
-                icon: Icons.card_giftcard_outlined,
-                iconShown: true,
-              ),
-              SizedBox(height: 10),
-              ProfileMenuItem(
-                text: 'Logout',
-                icon: Icons.logout,
-                iconShown: false,
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
           ],
         )),
   ));
